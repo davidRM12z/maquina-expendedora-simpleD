781f046e144ee459b92538da74e132a1d850e07b
David <davidrojomar12@gamil.com>
2022-11-08 20:07:09 +0100

    segundo constructor

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -23,7 +23,16 @@ public class MaquinaExpendedoraSimple {
         estacionOrigen = "León";
         estacionDestino = "Guardo";
     }
-
+    /**
+     * Segundo constructor
+     */
+    public MaquinaExpendedoraSimple(int precioDelBillete, String destino){
+        precioBillete = precioDelBillete;
+        balanceClienteActual = 0;
+        totalDineroAcumulado = 0;
+        estacionOrigen = "León";
+        estacionDestino = destino;        
+    }
     /**
      * Devuelve el precio del billete
      */
